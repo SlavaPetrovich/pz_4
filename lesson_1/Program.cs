6a59a113d05625aeb062dd33fafc7a78b65965ef
Slava <petrovichvecheslav@yandex.ru>
2022-10-21 17:59:09 +0300

    lesson_1

---
+++ lesson_1/Program.cs
@@ -0,0 +1,9 @@
+﻿void Method(int a, int b, int i)
+{
+    for (i = 0; i < 1; i++)
+    {
+    int c = Convert.ToInt32(Math.Pow(a, b));
+    Console.WriteLine($"{a},{b} -> {c}");
+    }
+}
+Method(23, 5, 0);
