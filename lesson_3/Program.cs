0858561f64f73e01e4b89498511c5d7b5965bbe7
Slava <petrovichvecheslav@yandex.ru>
2022-10-22 19:38:05 +0300

    lesson_3

---
+++ lesson_3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите массив чисел: ");
+string a = Console.ReadLine();
+string[] b = a.Split(",");
+int i;
+int c = new int[b.Length];
+
+for (i = 0; i < b.Length; i++)
+{
+    c[i] = int.Parse(b[i]);
+}
+
+for (i = 0; i < c.Length; i++)
+{
+    Console.Write($"c{i}, ");
+}
